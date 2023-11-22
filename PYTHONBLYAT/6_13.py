95d319ed92cfae2bfa174654d7504c15d02a6129
erifouD <artmandr@yandex.ru>
2023-11-22 19:53:35 +0300

    6_13 Fix

--- PYTHONBLYAT/6_13.py
+++ PYTHONBLYAT/6_13.py
@@ -1,3 +1,4 @@
+#FIRST TASK================================
 TArray = [0, 4, 5, 6, 7, 3, 1, 4, 6, 7, 8, 1, 3, 4, 5, 6, 2, 4, 5]
 b = int(input("Enter array manually?\n0 - no\n1 - yes\n"))
 if(b):
@@ -5,15 +6,14 @@ if(b):
     Num = int(input("Enter array size: "))
     for i in range(Num): TArray.append(int(input(f"Enter  {i + 1} item of array: ")))
 
-AlreadyChecked = []
 for i in range(min(TArray), max(TArray)):
-    if(not TArray.count(i) or i in AlreadyChecked): continue
+    if(TArray.count(i) < 2): continue
     print("Value:", i, end = ", Indexes: ")
     for j in range(len(TArray)): 
         if(TArray[j] == i): print(j, end = " ")
     print()
 
-#NEXT TASK===============================
+#SECOND TASK==============================
 print()
 TArray = [18, 5, 12, 21, 1, 3, 6, 9]
 b = int(input("Enter array manually?\n0 - no\n1 - yes\n"))
