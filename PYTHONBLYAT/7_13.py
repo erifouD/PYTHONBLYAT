25f907d64125bab7f19b375c8ee96a63e1e1fc4e
erifouD <artmandr@yandex.ru>
2023-12-05 12:08:34 +0300

    Привел свое задание в нормальный вид

--- PYTHONBLYAT/7_13.py
+++ PYTHONBLYAT/7_13.py
@@ -22,28 +22,30 @@ TotalWeight = 0
 ApplicationList = []
 CourierApplications = []
 
-'''for i in range(5):
+for i in range(5):
     Name = str(input("Введите название: "))
     Amount = int(input("Введите количество: "))
     Name = Name[0].capitalize() + Name.lower()[1:]
     TotalWeight += Amount
     ApplicationList.append(InputFruit(Name, Amount))
-'''
+
 
 #Debug Stuff=====================================
-ApplicationList = [
+
+'''ApplicationList = [
         InputFruit("Апельсин", 4),
         InputFruit("Груша", 3),
         InputFruit("Арбуз", 23),
         InputFruit("Авокадо", 2),
         InputFruit("Дыня", 2)
         ]
-TotalWeight = 34
+TotalWeight = 34'''
+
 #End Debug Stuff=================================
 
 if(TotalWeight % 9 == 0): CourierNumber = TotalWeight / 9
 else: CourierNumber = TotalWeight // 9 + 1
-
+print("============================================================")
 for i in range(CourierNumber):
     #Итерация цикла - заполнение заявки для курьера
 
@@ -90,7 +92,7 @@ for i in CourierApplications:
     print(f"Заявка для {CourierApplications.index(i) + 1}-го курьера:")
     for Key, Value in i.items():
         print(f"\t {Key} - {Value}кг.")
-    print()
+    print("============================================================")
 
 print("\nОстатки на складе:\nНаименование\t№Склада\tВес\tСтоимость за кг")
 for Key, Value in FruitsData.items():
