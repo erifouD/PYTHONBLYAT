02a3f55c0853bccd5be2de5c9de1a68fb6885ac7
erifouD <artmandr@yandex.ru>
2023-12-05 11:21:31 +0300

    Егор, 7 лаба

---
+++ PYTHONBLYAT/7_5.py
@@ -0,0 +1,25 @@
+﻿UsersList = []
+
+def input_data():
+    UserData = {
+        "name" : str(input("Введите имя: ")),
+        "surname" : str(input("Введите фамилию: ")),
+        "age" : int(input("Введите возраст: ")),
+        }
+    return UserData
+
+def add_user(UserData):
+    UsersList.append(UserData)
+
+
+for i in range (6):
+    add_user(input_data())
+
+MaxAge = {"name" : "none", "surname": "none", "age" : 0}
+for i in UsersList:
+    if i.get("age") > MaxAge.get("age"):
+        MaxAge = i
+
+print("Пользователь с наибольшим возрастом:\n")
+for key, value in MaxAge.items():
+    print(f"{key}: {value}")
